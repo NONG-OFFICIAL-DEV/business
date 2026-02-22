705af266c5bc544229ba5cd7ef90e855441c2853
ONG-PHLOEUT-GITHUB <phloeutnong@gmail.com>
2026-02-22 21:42:45 +0700

    web socket

--- frontend/src/utils/echo.js
+++ frontend/src/utils/echo.js
@@ -1,19 +1,16 @@
-import Echo from 'laravel-echo'
-import Pusher from 'pusher-js'
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
-window.Pusher = Pusher
-// Disable Pusher's default logging
-Pusher.logToConsole = false
+window.Pusher = Pusher;
 
 const echo = new Echo({
-  broadcaster: 'reverb',
-  key: import.meta.env.VITE_REVERB_APP_KEY,
-  wsHost: import.meta.env.VITE_REVERB_HOST,
-  wsPort: import.meta.env.VITE_REVERB_PORT,
-  wssPort: import.meta.env.VITE_REVERB_PORT,
-  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
-  enabledTransports: ['ws', 'wss'], // ← only use WebSocket, skip other transports
-  disableStats: true // ← stop pinging Pusher stats servers
-})
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
+    enabledTransports: ['ws', 'wss'],
+});
 
-export default echo
+export default echo;
